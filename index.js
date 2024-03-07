e8a91cae8c903c30dad6dc7501db7ab05969767e
Lenka Michaleová <michaleova.lenka@gmail.com>
2024-03-07 02:49:23 +0100

    vyreseno

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const question = document.querySelector(".question")
+const answerText = document.querySelector(".answer__text")
+const answer = document.querySelector(".answer")
+const answerUser = prompt("Kdo má vždycky pravdu?")
 
+question.textContent = "Kdo má vždycky pravdu?"
+answerText.textContent = answerUser
+
+if (answerUser == "Lenka") {
+    answer.classList.add("answer--correct")
+} else {
+    answer.classList.add("answer--wrong")
+}
